20478d142587e1ac0831d12d787a0111e6e772a2
aleksey <alekseyt@shtrafy-gibdd.ru>
2021-07-26 23:26:32 +0300

    add json and md for print

--- src/githab.com/AlekseyTroshin/Json/sohamkamani/008.go
+++ src/githab.com/AlekseyTroshin/Json/sohamkamani/008.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 )
 
 func main() {
-	
+	birdJson := `{"birds":{"pigeon":"likes to perch on rocks","eagle":"bird of prey"},"animals":"none"}`
+	var result map[string]interface{}
+	json.Unmarshal([]byte(birdJson), &result)
+
+	bird := result["birds"].(map[string]interface{})
+
+	for key, j := range bird {
+		fmt.Println(key, j.(string))
+	}
 }
